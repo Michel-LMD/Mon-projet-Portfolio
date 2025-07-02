39cd7b636a72fa2f85bb4cafd86eb11246d33559
Michel-LMD <limaziemichel2909@gmail.com>
2025-07-02 21:15:40 +0200

    Add files via upload

--- Scripts.js
+++ Scripts.js
@@ -44,3 +44,20 @@ if (form.message.value ==="") {
 alert("Formulaire validé avec succès!");
 return true;
 }
+
+/*//Télécharger de fichiers
+document.getElementById('downloadCV').addEventListener('click', function() {
+    // Créer un lien invisible
+    const link = document.createElement('a');
+    link.href = 'fichiers pdf\Dondina_LIMAZIE MAZIGNADA_CV+.pdf';
+    link.download = 'Dondina_LIMAZIE MAZIGNADA_CV+.pdf'; // Nom du fichier téléchargé
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
+
+document.getElementById('downloadCV').addEventListener('click', function() {
+    if(confirm('Voulez-vous télécharger mon CV ?')) {
+         window.open('fichiers pdf\Dondina_LIMAZIE MAZIGNADA_CV+.pdf', '_blank')
+    }
+});*/
